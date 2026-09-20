8200af8d3e4b605c533ead06eceda302d2e45ccd
mrsBungleO <0brisaliconarossetti0@ciencias.unam.mx>
2026-09-20 14:06:33 -0600

    Solo agregué los override que eliminaron de una clase. Chequen la documentacion y sus diagramas por favor

--- PizzaDeMariscos.java
+++ PizzaDeMariscos.java
@@ -13,6 +13,7 @@ public class PizzaDeMariscos extends Pizza{
     /**
      * método que sobreescribe el método hook para un caso donde la pizza contiene un queso distinto al mozzarella (manchego).
      */
+    @Override
     public void colocarQuesoConHook(){
         System.out.println("El robot Cesarín agrega queso manchego rayado a la masa.");
     }
@@ -20,6 +21,7 @@ public class PizzaDeMariscos extends Pizza{
     /**
      * implemetación del método abstracto colocarProteina, en este caso para que aplique los camarones y el pulpo a la masa.
      */
+    @Override
     public void colocarProteina(){
         System.out.println("El robot Cesarín agrega algunos camarones y un poco de pulpo en trozos a la masa de manera uniforme.");
     }
